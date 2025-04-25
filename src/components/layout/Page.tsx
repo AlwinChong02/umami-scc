cb43945c2a81fb3bbff42f0520b57584cc8bb907
Chung Wei Seng <130556821+WeiSengChung@users.noreply.github.com>
2025-04-25 22:42:15 +0800

    Update Page.tsx

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -4,7 +4,7 @@ import classNames from 'classnames';
 import { Banner, Loading } from 'react-basics';
 import { useMessages } from '@/components/hooks';
 import styles from './Page.module.css';
-// Add a Comment to trigger local Jenkins pipeline
+// Add a Comment to trigger local Jenkins pipeline #2
 export function Page({
   className,
   error,
